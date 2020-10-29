1e465a50554b052930f1bac24ff5b397de22e1e1
Andrew Kallmeyer <ask@ask.systems>
2020-10-28 20:38:33 -0700

    Fix bootstrap-hex after the tester bootloader changes (oops)

--- bootstrap-hex.asm
+++ bootstrap-hex.asm
@@ -37,37 +37,11 @@
 %define CURSOR_ON_SECOND_NIBBLE 0b01
 %define SAVED_NIBBLE_AT_END 0b10
 
-%include "util/bootsect-header.asm"
+%include "bootloader/bootsect.asm"
 
-mov [BOOT_DISK], dl ; Save the boot disk number
-
-; Set video mode, 16 color 80x25 chars
-;
-; The IBM BIOS manual describes a long procedure for determining which video
-; modes are supported and all the possible options for supporting both mono and
-; color. Sometimes mono isn't supported if the PC supports color modes. So, I'm
-; just going to assume all modern hardware supports color modes.
-mov ax, 0x0003
-int 0x10
-
-; Make the 8th bit of the colors bg-intensity instead of blink
-mov ax, 0x1003
-mov bl, 0
-int 0x10
-
-; Make the 4th bit of the colors fg-intensity instead of font selection
-; Use block 0 for the font
-; Apparently this is the default in VGA so maybe we don't need it
-mov ax, 0x1103
-mov bl, 0
-int 0x10
-
-; Color byte is: bg-intensity,bg-r,bg-g,bg-b ; fg-intensity,fg-r,fg-g,fg-b
-
-; Set cursor shape to an underline
-mov ax, 0x0100
-mov cx, 0x0607
-int 0x10
+; This is also the start for calculating NUM_EXTRTA_SECTORS
+; There must be nothing other than the bootsector above this label
+start_:
 
 ; Set the border color (by clearing the whole screen)
 mov ax, 0x0600
@@ -82,98 +56,6 @@ mov cx, MAIN_TOP_LEFT
 mov dx, MAIN_BOTTOM_RIGHT
 mov bh, MAIN_COLOR
 int 0x10
-
-; Set the keyboard repeat speed
-mov ax, 0x0305
-;mov bx, 0x0107 ; 500 ms delay before repeat ; 16 characters per second
-mov bx, 0x0100 ; 500 ms delay before repeat ; 30 characters per second
-int 0x16
-
-; Load the code from the extra sectors
-mov ah, 0x02
-mov al, NUM_EXTRA_SECTORS
-mov bx, SECTOR_SIZE ; es:bx is address to write to. es = cs, so write directly after the boot sector
-mov cx, 0x0002 ; Cylinder 0; Sector 2 (1 is the boot sector)
-mov dl, [BOOT_DISK]
-xor dh, dh ; Head 0
-int 0x13
-
-; Check for errors
-cmp ax, NUM_EXTRA_SECTORS
-je start_
-
-push ax ; push the error code
-
-; Print the error message
-mov ax, 0x1301 ; Write String, move cursor mode in al
-mov bp, error_msg ; String pointer in es:bp (es is at code start from bootsect-header.asm)
-mov cx, error_msg_len ; String length
-mov dx, MAIN_TOP_LEFT
-mov bx, MAIN_COLOR ; bh = 0 (page number); bl = color
-int 0x10
-
-pop cx ; pop the error code
-call print_hex ; print the error code
-
-jmp $ ; stop forever
-
-; Prints the ascii hex character which represents the integer value of al
-; Only accepts 0x0 <= al <= 0xF, anything else is garbage output
-; e.g. al = 12 prints "C"
-; clobbers ax, and bx
-print_hex_char:
-  mov ah, 0x0E
-  xor bx, bx
-  ; fallthrough
-; Also assumes ah = 0x0E and bx = 0
-_print_hex_char:
-  cmp al, 9
-  ja .over_9
-
-  add al, '0'
-  int 0x10
-  ret
-
-.over_9:
-  sub al, 10
-  add al, 'A'
-  int 0x10
-  ret
-
-; cx = two bytes to write at current cursor
-; clobbers ax, and bx
-print_hex:
-  mov ah, 0x0E ; Scrolling teletype BIOS routine (used with int 0x10)
-  xor bx, bx ; Clear bx. bh = page, bl = color
-
-  ; Nibble 0 (most significant)
-  mov al, ch
-  shr al, 4
-  call _print_hex_char
-  ; Nibble 1
-  mov al, ch
-  and al, 0x0F
-  call _print_hex_char
-  ; Nibble 2
-  mov al, cl
-  shr al, 4
-  call _print_hex_char
-  ; Nibble 3
-  mov al, cl
-  and al, 0x0F
-  call _print_hex_char
-
-  ret
-
-error_msg: db `Error reading additional sectors from disk: `
-error_msg_len: equ $-error_msg
-
-BOOT_DISK: db 0x00 ; value is filled first thing
-
-%include "util/bootsect-footer.asm"
-; This is also the start for calculating NUM_EXTRTA_SECTORS
-; There must be nothing other than the bootsector above this label
-start_:
 
 ; Print the starting greeting
 mov ax, 0x1301 ; Write String, move cursor mode in al
@@ -614,8 +496,4 @@ row_header_len: equ $-row_header
 run_instr: db `Press Ctrl+D to run your code immediately.`
 run_instr_len: equ $-run_instr
 
-; The -1 is because we don't want the next sector until we have 512+1 bytes
-; e.g. for exactly 512 bytes we want 1 extra sector not 2
-;
-; The +1 is because int division does floor() and we want ceil()
-NUM_EXTRA_SECTORS: equ ($-start_-1)/SECTOR_SIZE + 1
+NUM_EXTRA_SECTORS: equ NUM_SECTORS(start_)
