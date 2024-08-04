f764d45fe916ca1e9c52648f03d7027a2fa09a2f
Andy Kallmeyer <ask@ask.systems>
2024-08-03 17:24:13 -0700

    Add a new debug text lisp program that defines numbers!
    I wrote this last weekend I'm pretty sure.

--- bootstrap-lisp.asm
+++ bootstrap-lisp.asm
@@ -39,16 +39,43 @@ debug_text:
 ;db "(define elm2 (lambda (l) (car (cdr l)))) (elm2 '(foo bar baz))", 0
 ;db "(quote ((l) . (car (cdr l))))",`\n`,'((l) . (car (cdr l)))", 0
 ;db "(define append1 (lambda (s t) (cond ((pair? s) (cons (car s) (append1 (cdr s) t))) (#t t) ))) (append1 '(this is a) '(test))", 0
-db "(define append1 (lambda (s t)",`\n`
+
+;db "(define append1 (lambda (s t)",`\n`
+;db "  (cond",`\n`
+;db "    ((pair? s) (cons (car s) (append1 (cdr s) t)))",`\n`
+;db "    (#t t)",`\n`
+;db "  )",`\n`
+;db "))",`\n`,`\n`
+;db "(define greeting (lambda name",`\n`
+;db "   (append1 '(Hello, ) name)",`\n`
+;db "))",`\n`,`\n`
+;db "(greeting 'lisp)",0
+
+db "(define 0 ())",`\n`
+db "(define succ (lambda (x) (cons x 0)))",`\n`
+db "(define 1 (succ 0))"," "
+db "(define 2 (succ 1))"," "
+db "(define 3 (succ 2))",`\n`
+db "(define dec (lambda (x) (car x)))",`\n`
+db "(define rep (lambda (x c)",`\n`
 db "  (cond",`\n`
-db "    ((pair? s) (cons (car s) (append1 (cdr s) t)))",`\n`
-db "    (#t t)",`\n`
+db "    ((eq? c 0) ())",`\n`
+db "    (#t (cons x (rep x (dec c)) ))",`\n`
 db "  )",`\n`
-db "))",`\n`,`\n`
-db "(define greeting (lambda name",`\n`
-db "   (append1 '(Hello, ) name)",`\n`
-db "))",`\n`,`\n`
-db "(greeting 'lisp)",0
+db "))",`\n`
+;db "(rep 'lisp 3)",`\n`,`\n`
+
+db "(define add (lambda (a b)",`\n`
+db "  (cond",`\n`
+db "   ((eq? b 0) a)",`\n`
+db "   (#t (succ (add a (dec b))))",`\n`
+db "  )",`\n`
+db "))",`\n`
+db "(define 5 (add 3 2))",`\n`
+db "(rep 'lisp 5)",0
+
+;db "(eq? '(a b c) '(cons a (b c)))",0
+
 ;db "(eq? 'a 'a) (eq? 'a 'b) (eq? #t #t) (eq? () ()) (define foo '(test me)) (eq? foo foo) (eq? '(foo) '(foo))", 0
 %endif
 
@@ -235,7 +262,6 @@ prim_cons:
   push ax
   push dx
 
-
   ; The second arg to cons
   call cdr
   call car
@@ -336,6 +362,7 @@ prim_eq:
 ;     ...
 ;   )
 prim_cond:
+  ; TODO: check the formatting of the arguments and return errors otherwise!
   .orig: equ objsize
   push ax
   push dx
@@ -390,6 +417,7 @@ prim_cond:
 ; which is printed as: (because there's no way to distingish a cons from a list start)
 ;  (((l) car (cdr l)))
 prim_lambda:
+  ; TODO: should error if there's more than one value not ignore it
   mov bp, sp
   .orig: equ 4
   push ax
